6af688fd772a6f96ae3985ed1d810782eb1f8779
piniondevs <sucmanuts420@gmail.com>
2021-10-16 06:57:32 +0000

    Finished giving the bot abilittyto add requests

--- commands/authorize.js
+++ commands/authorize.js
@@ -1,6 +1,14 @@
-const ErrorEmbed = require('../utils/errorEmbed');
+const axios = require('axios');
+const dotenv = require('dotenv');
 
-module.exports = (message) => {
+dotenv.config();
+
+const endpoint = process.env.ENDPOINT;
+
+const ErrorEmbed = require('../utils/errorEmbed');
+const InfoEmbed = require('../utils/infoEmbed');
+
+module.exports = async (message) => {
     try {
         if (message.author.id !== '453146976008011777') {
             message.channel.send(new ErrorEmbed('Unauthorized', 'Yeah m8 you arent authorized to do that.'));
@@ -17,6 +25,14 @@ module.exports = (message) => {
             return;
         }
 
+        const res = await axios.post(`${endpoint}/requests/accept`, { id: id });
+
+        if (res.data === 'Well shit, we couldnt find that request in our database.') {
+            message.channel.send(new ErrorEmbed('Wrong ID', 'We could not find a request with that id in our database.'));
+            return;
+        }
+
+        message.channel.send(new InfoEmbed('Gali Authorized', 'The gali has been authorized.'));
     } catch (err) {
         const someRandomErr = new ErrorEmbed('Something Went Wrong', 'Something went horribly wrong, ping tahlil so that he can check the logs and find out what it is.');
         message.channel.send(someRandomErr);
