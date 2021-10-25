279dd81f2fb084425ee84e56a755ce3ed1d52cf5
piniondevs <sucmanuts420@gmail.com>
2021-10-25 04:12:28 +0000

    Not making a changelog

--- commands/changelog.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'changelog',
-    helpInfo: 'Shows the bots changelog and all the various things happening with it.',
-    handler: (message) => {
-        message.channel.send(message.content);
-    }
-}
